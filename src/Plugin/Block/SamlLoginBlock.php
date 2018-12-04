326adf891406c9920afb2acd5a225163c362a419
Mike Decker <pookmish@gmail.com>
2018-12-04 14:41:30 -0800

    login block

---
+++ src/Plugin/Block/SamlLoginBlock.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Drupal\stanford_ssp\Plugin\Block;
+
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Link;
+use Drupal\Core\Session\AccountInterface;
+
+/**
+ * Provides a 'SamlLoginBlock' block.
+ *
+ * @Block(
+ *  id = "saml_login_block",
+ *  admin_label = @Translation("Saml login block"),
+ * )
+ */
+class SamlLoginBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return ['link_text' => 'SUNetID Login'] + parent::defaultConfiguration();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $form['link_text'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Text of the SimpleSAML PHP login link'),
+      '#description' => $this->t('Here you can replace the text of the SimpleSAML PHP link.'),
+      '#default_value' => $this->configuration['link_text'],
+      '#required' => TRUE,
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function access(AccountInterface $account, $return_as_object = FALSE) {
+    $access = AccessResult::allowedIf($account->isAnonymous());
+    return $return_as_object ? $access : $access->isAllowed();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    $this->configuration['link_text'] = $form_state->getValue('link_text');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = [];
+    $build['saml_link'] = Link::createFromRoute($this->configuration['link_text'], 'simplesamlphp_auth.saml_login')
+      ->toRenderable();
+    return $build;
+  }
+
+}
