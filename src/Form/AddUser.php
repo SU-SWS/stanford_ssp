aca185222f37c87d29d057f02f8d0e66e1780762
Mike Decker <pookmish@gmail.com>
2018-12-05 13:49:18 -0800

    working add user form

---
+++ src/Form/AddUser.php
@@ -0,0 +1,171 @@
+<?php
+
+namespace Drupal\stanford_ssp\Form;
+
+use Drupal\Component\Utility\Html;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\externalauth\AuthmapInterface;
+use Drupal\user\Entity\User;
+use Drupal\user\RoleInterface;
+use Egulias\EmailValidator\EmailValidatorInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Class AddUser
+ *
+ * @package Drupal\stanford_ssp\Form
+ */
+class AddUser extends FormBase {
+
+  /**
+   * External authmap service.
+   *
+   * @var \Drupal\externalauth\AuthmapInterface
+   */
+  protected $authmap;
+
+  /**
+   * Email Validator service.
+   *
+   * @var \Egulias\EmailValidator\EmailValidatorInterface
+   */
+  protected $emailValidator;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('externalauth.authmap'),
+      $container->get('email.validator')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(AuthmapInterface $authmap, EmailValidatorInterface $email_validator) {
+    $this->authmap = $authmap;
+    $this->emailValidator = $email_validator;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'stanford_ssp_add_user';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form = [];
+    $form['sunetid'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('SUNetID'),
+      '#description' => $this->t('Enter the SUNetID of the user you wish to add.'),
+      '#required' => TRUE,
+      '#default_value' => 'pookmish',
+    ];
+    $form['name'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Name'),
+      '#description' => $this->t('If you wish to specify the user\'s preferred name (instead of sunetid@stanford.edu), enter it here.'),
+    ];
+    $form['email'] = [
+      '#type' => 'email',
+      '#title' => $this->t('Email Address'),
+      '#description' => $this->t('If you wish to specify an alternate email address (instead of sunetid@stanford.edu), enter it here.'),
+    ];
+    if ($this->config('simplesamlphp_auth.settings')
+      ->get('allow.set_drupal_pwd')) {
+      $form['pass'] = [
+        '#type' => 'password_confirm',
+        '#size' => 25,
+      ];
+    }
+    $roles = user_role_names();
+    unset($roles[RoleInterface::ANONYMOUS_ID], $roles[RoleInterface::AUTHENTICATED_ID]);
+    $form['roles'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Roles'),
+      '#description' => $this->t('Add roles to the new user account.'),
+      '#options' => $roles,
+      '#multiple' => TRUE,
+    ];
+    $form['notify'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Notify user of new account'),
+    ];
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Add SSO User'),
+    ];
+    return $form;
+  }
+
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    parent::validateForm($form, $form_state);
+    $sunet = strtolower(trim(Html::escape(($form_state->getValue('sunetid')))));
+    $form_state->setValue('sunetid', $sunet);
+
+    if ($this->authmap->getUid($sunet, 'simplesamlphp_auth')) {
+      $form_state->setError($form['sunetid'], $this->t('Could not create user. Authname %name already exists. Has the user already been created with a different username but the same SUNetID?', ['%name' => $sunet]));
+    }
+
+    // If no name is specified, use the default name (sunetid + @stanford.edu).
+    $name = trim(Html::escape($form_state->getValue('name'))) ?: $sunet;
+    $form_state->setValue('name', $name);
+
+    // Check that there is no user with the same name.
+    if (user_load_by_name($name)) {
+      $form_state->setError($form['name'], $this->t('Could not create user. Username %name already exists.', ['%name' => $name]));
+    }
+
+    // If no email was specified, we'll use the default (sunetid + @stanford.edu).
+    $default_email = $sunet . '@stanford.edu';
+    $email = strtolower(trim($form_state->getValue('email'))) ?: $default_email;
+    $form_state->setValue('email', $email);
+
+    if (!$this->emailValidator->isValid($email)) {
+      $form_state->setError($form['email'], t('The e-mail address %email is not valid.', ['%email' => $email]));
+    }
+
+    // Check that there is no user with the same email
+    // Drupal will let us create the user with a duplicate email, but
+    // the user will run into issues when making changes to their profile.
+    if (user_load_by_mail($email)) {
+      $form_state->setError($form['email'], $this->t('Could not create user. Email %email already in use.', ['%email' => $email]));
+    }
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $new_user = User::create([
+      'name' => $form_state->getValue('name'),
+      'pass' => user_password(),
+      'mail' => $form_state->getValue('email'),
+      'roles' => array_values($form_state->getValue('roles')),
+      'status' => 1,
+    ]);
+    $success = $new_user->save();
+
+    $this->authmap->save($new_user, 'simplesamlphp_auth', $form_state->getValue('sunetid'));
+    $this->messenger()
+      ->addStatus($this->t('Successfully created SSO account for %user', ['%user' => $new_user->getUsername()]));
+    $this->logger('stanford_ssp')
+      ->info('Created User %name', ['%name' => $new_user->getUsername()]);
+
+    // Was the notify checkbox checked?
+    if ($form_state->getValue('notify') && $success) {
+      _user_mail_notify('register_admin_created', $new_user);
+      $this->messenger()->addStatus($this->t('Email sent to user'));
+    }
+  }
+
+}
