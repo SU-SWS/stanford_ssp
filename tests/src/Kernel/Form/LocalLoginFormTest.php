72c27c535a0ab33d6402231dd373c21a7d0beb3b
Mike Decker <pookmish@gmail.com>
2019-08-23 13:38:42 -0700

    local login form test

---
+++ tests/src/Kernel/Form/LocalLoginFormTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Drupal\Tests\stanford_ssp\Kernel\Form;
+
+use Drupal\Core\Form\FormState;
+use Drupal\KernelTests\KernelTestBase;
+use Drupal\user\Entity\Role;
+
+class LocalLoginFormTest extends KernelTestBase {
+
+  /**
+   * {@inheritDoc}
+   */
+  protected static $modules = [
+    'system',
+    'user',
+    'stanford_ssp',
+    'simplesamlphp_auth',
+    'externalauth',
+  ];
+
+  /**
+   * {@inheritDoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+    $this->installEntitySchema('user');
+    $this->installEntitySchema('user_role');
+    $this->installSchema('externalauth', 'authmap');
+    $this->installSchema('system', ['key_value_expire', 'sequences']);
+
+    for ($i = 0; $i < 5; $i++) {
+      Role::create(['label' => "Role $i", 'id' => "role$i"])->save();
+    }
+    \Drupal::configFactory()
+      ->getEditable('simplesamlphp_auth.settings')
+      ->set('allow.default_login_roles', [])
+      ->save();
+  }
+
+  /**
+   * Make sure the form saves the config.
+   */
+  public function testLoginFormConfig() {
+    $this->assertFalse(\Drupal::config('stanford_ssp.settings')
+      ->get('hide_local_login'));
+    $form = \Drupal::formBuilder()
+      ->getForm('\Drupal\stanford_ssp\Form\LocalLoginForm');
+
+    $this->assertCount(26, $form);
+    $form_state = new FormState();
+    $form_state->setValue('hide_local_login', TRUE);
+    \Drupal::formBuilder()
+      ->submitForm('\Drupal\stanford_ssp\Form\LocalLoginForm', $form_state);
+
+    $this->assertTrue(\Drupal::config('stanford_ssp.settings')
+      ->get('hide_local_login'));
+  }
+
+}
